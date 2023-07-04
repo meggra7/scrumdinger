0e890725ca9abbd90e3fa773dea18f3411a64f1d
Meghan Dunham <meghan.g.dunham@gmail.com>
2023-07-04 14:32:58 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Life Boss on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
